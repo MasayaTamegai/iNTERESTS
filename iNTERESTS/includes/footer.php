b0aea0822cea033458fd9fbcc48a6df56e373f72
Temet Nosce <mtamegai@gmail.com>
2017-01-13 16:51:20 -0600

    Add files via upload

---
+++ iNTERESTS/includes/footer.php
@@ -0,0 +1,1 @@
+<footer>Masaya Tamegai</footer>
---
+++ iNTERESTS/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Balance</title>
+</head>
+<body>
+   
+   <h1>Things I keep interest in to stay sane.</h1>
+   <p>I keep my <a href="body/body.html" target="_blank">body</a> strong.</p>
+    <p>I keep my <a href="mind/mind.html" target="_blank">mind</a> sharp.</p>
+    <p>My <a href="soul/soul.html" target="_blank">soul</a> at ease.</p>
+    
+</body>
+
+<?php include "includes/footer.php"; ?>
+
+</html>
